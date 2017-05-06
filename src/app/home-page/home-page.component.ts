2dabf1424c2c31f94496b3b404ff38339a189944
devilzripper <markuswagner93@gmail.com>
2017-05-06 23:22:43 +0200

    More Details

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ConfigService } from '../services/config.service';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-
-  constructor() { }
-
+  configService = null;
+  constructor(configService : ConfigService) {
+    this.configService = configService;
+   }
   ngOnInit() {
   }
 
