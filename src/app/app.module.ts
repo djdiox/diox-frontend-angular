63126d4f5f2aedb4587480992db927802fa1f8d3
devilzripper <markuswagner93@gmail.com>
2017-05-06 16:50:48 +0200

    first work done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { GalleriaModule } from 'primeng/primeng';
+import { AccordionModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
@@ -13,6 +17,9 @@ import { BiographyComponent } from './biography/biography.component';
 import { ContactComponent } from './contact/contact.component';
 import { FootBarComponent } from './foot-bar/foot-bar.component';
 import { SettingsComponent } from './settings/settings.component';
+import { PhotosComponent } from './photos/photos.component';
+import { CodeComponent } from './code/code.component';
+import { StreamComponent } from './stream/stream.component';
 
 @NgModule({
   declarations: [
@@ -24,20 +31,26 @@ import { SettingsComponent } from './settings/settings.component';
     BiographyComponent,
     ContactComponent,
     FootBarComponent,
-    SettingsComponent
+    SettingsComponent,
+    PhotosComponent,
+    CodeComponent,
+    StreamComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    GalleriaModule,
+    AccordionModule,
+    BrowserAnimationsModule
   ],
   providers: [ConfigService],
   bootstrap: [AppComponent]
 })
-export class AppModule {   
+export class AppModule {
   configService = null;
   currentView = '';
-    constructor(configService: ConfigService) {
+  constructor(configService: ConfigService) {
     this.configService = configService;
   }
 }
